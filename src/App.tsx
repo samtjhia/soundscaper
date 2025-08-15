7c75810803992a27474e36349e71e4f0c5a2ba7c
Samuel Tjhia <110407645+samtjhia@users.noreply.github.com>
2025-08-15 15:47:43 -0400

    chore: init project w vite react ts tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-gray-950 text-gray-100">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight">SoundSketch</h1>
+        <p className="mt-2 text-sm text-gray-300">
+          WE IN!!!!
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
